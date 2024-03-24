8e50944148171f3db7dbff7b996bb39c1ac91756
Benas Puskunigis <85058665+Paccomo@users.noreply.github.com>
2024-03-24 14:14:35 +0200

    Register form, input validation, save `person` model

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,29 +3,54 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Models\Person;
+use App\Constants\Role;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 use App\Models\Account;
 
+// TODO Translations
+// TODO form rearrange
+// TODO Only admin and director register
+// TODO enums by role
 class RegisterController extends Controller
 {
-   public function register(Request $request) {
+    public function register(Request $request) {
         $request->validate([
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:account'],
+            'email' => ['required', 'string', 'email', 'max:255'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'role' => [Rule::enum(Role::class)],
+            'name' => ['required', 'regex:/^[\p{L} ]+$/u'],
+            'surname' => ['required', 'regex:/^[\p{L} -]+$/u'],
+            'pid' => ['required', 'integer', 'digits:11'],
+            'address' => ['required', 'regex:/^[\p{L}. \-\d]+$/u'],
+            'city' => ['required', 'regex:/^[\p{L} ]+$/u'],
+            'phoneNum' => ['required', 'regex:/^(8|\+370)\s?6\s?\d(?:\s?\d){6}$/'],
         ]);
 
-        $acc = Account::create([
+        $account = Account::create([
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => $request->role,
         ]);
 
-        return redirect()->route('login');
-   }
+        $person = new Person();
+        $person->id = $account->id;
+        $person->name = encrypt($request->name);
+        $person->surname = encrypt($request->surname);
+        $person->pid = encrypt($request->pid);
+        $person->address = encrypt($request->address . ", " . $request->city);
+        $person->phone_number = encrypt($request->phoneNum);
+        $person->save();
 
-   public function showRegistrationForm() {
-        return view('auth.register');
-   }
+        // Create either client or employee
+
+        return redirect()->route('login');
+    }
+
+    public function showRegistrationForm() {
+        $roles = array_combine(array_column(Role::cases(), 'value'), array_column(Role::cases(), 'name'));
+        return view('auth.register', ['roles' => $roles]);
+    }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,6 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -44,6 +43,108 @@
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Rolė') }}</label>
+
+                            <div class="col-md-6">
+                                <select name="role" class="form-select @error('role') is-invalid @enderror" id="role" required>
+                                    @foreach ($roles as $value => $displayName)
+                                        <option value="{{ $value }}">{{ $displayName }}</option>
+                                    @endforeach
+                                </select>
+
+                                @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" required autocomplete="given-name">
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="surname" class="col-md-4 col-form-label text-md-end">{{ __('Surame') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" required autocomplete="family-name">
+
+                                @error('surname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="pid" class="col-md-4 col-form-label text-md-end">{{ __('Asmens kodas') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="pid" type="text" class="form-control @error('pid') is-invalid @enderror" name="pid" required>
+
+                                @error('pid')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="address" class="col-md-4 col-form-label text-md-end">{{ __('Adresas') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" required autocomplete="street-address">
+
+                                @error('address')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="city" class="col-md-4 col-form-label text-md-end">{{ __('Miestas') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="city" type="text" class="form-control @error('city') is-invalid @enderror" name="city" required autocomplete="address-level2">
+
+                                @error('city')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="phoneNum" class="col-md-4 col-form-label text-md-end">{{ __('Tel. nr.') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="phoneNum" type="tel" class="form-control @error('phoneNum') is-invalid @enderror" name="phoneNum" required autocomplete="tel">
+
+                                @error('phoneNum')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
 
